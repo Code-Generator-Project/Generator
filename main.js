027191b26e85c3a737b5546e796d993e4667ee93
Wassim El Fen <wassimelfen@gmail.com>
2021-10-14 12:05:36 +0100

    input values

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-
-var arrayStyle=[]
+var arrayStyle = [];
 function changeStyle(n) {
-    $("link").attr("href",arrayStyle(n))
+  $("link").attr("href", arrayStyle(n));
 }
 
 $(".form-container").hide();
@@ -9,16 +8,11 @@ function style1() {
   $("table").hide(1000);
   $(".form-container").show(1500);
 }
-function style2() {
-  $("table").hide(1000);
-  $(".form-container").show(1500);
-}
-function style3() {
-  $("table").hide(1000);
-  $(".form-container").show(1500);
-}
-function style4() {
-  $("table").hide(1000);
-  $(".form-container").show(1500);
-}
 
+var cnID = $("#cnID").val();
+var descID = $("#descID").val();
+var servID = $("#servID").val();
+var imgID = $("#imgID").val();
+var car1ID = $("#car1ID").val();
+var car2ID = $("#car2ID").val();
+var car3ID = $("#car3ID").val();
