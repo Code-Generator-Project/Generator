48135ee0a76dbcae721507e8602453f0233f44b7
Wassim El Fen <wassimelfen@gmail.com>
2021-10-14 10:08:38 +0100

    home page and form

---
+++ main.js
@@ -0,0 +1,17 @@
+$(".form-container").hide();
+function style1() {
+  $("table").hide(1000);
+  $(".form-container").show(1500);
+}
+function style2() {
+  $("table").hide(1000);
+  $(".form-container").show(1500);
+}
+function style3() {
+  $("table").hide(1000);
+  $(".form-container").show(1500);
+}
+function style4() {
+  $("table").hide(1000);
+  $(".form-container").show(1500);
+}
