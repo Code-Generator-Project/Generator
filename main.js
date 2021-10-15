308cd5bf171e70461ca7a765bbdc333f41107bb7
BelguesmiSakr <belguesmi_sakr@yahoo.com>
2021-10-15 02:39:06 +0100

    first template done

--- main.js
+++ main.js
@@ -14,18 +14,13 @@ var slideIndex = 0;
 showSlides();
 
 $(".form-container").hide();
+$("#company-infos").hide();
 function style1() {
   $("table").hide(1500);
   $(".form-container").show(1500);
 }
 
-var cnID = $("#cnID").val();
-var descID = $("#descID").val();
-var servID = $("#servID").val();
-var imgID = $("#imgID").val();
-var car1ID = $("#car1ID").val();
-var car2ID = $("#car2ID").val();
-var car3ID = $("#car3ID").val();
+
 function showSlides() {
   var i;
   var slides = $(".myslide");
@@ -40,9 +35,39 @@ function showSlides() {
   setTimeout(showSlides, 2500); // Change image every 2.5 seconds
 }
 
-$(".form-container").hide();
 function showForm() {
   $("table").hide(1000);
   $("#caroussel-container").hide(1000);
   $(".form-container").show(1500);
 }
+
+// function to change the img src of the slide show
+var imgIds=["#img1","#img2","#img3","#img4"]
+function slideShowUpdate(array) {
+  for (let i = 0; i < array.length; i++) {
+    $(imgIds[i]).attr("src",array[i])  
+  }
+}
+
+// function to fill the tamplate elements with the input values 
+function showTemplate() {
+  $(".form-container").hide();
+  changeStyle(1)
+  $("#cnID-template").text($("#cnID").val())
+  $("#desID-template").text($("#descID").val())
+  $("#servID-template").text($("#servID").val())
+  $("#imgID-template").attr("src",$("#imgID").val())
+  slideShowUpdate([$("#car1ID").val(),$("#car2ID").val(),$("#car3ID").val(),$("#car4ID").val()])
+  $("#caroussel-container").show(1000);
+  $("#company-infos").show(1000);
+}
+
+$("#code-container").hide()
+// function that generate the template html code
+function generateCode() {
+  $("#code-container").text($("#company-infos")[0].outerHTML)
+  $("#code-container").show(2000)
+}
+
+
+
